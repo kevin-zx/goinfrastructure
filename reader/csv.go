3dc8b854b5ccff088c40a4244e3490a290ae049f
kevin-zx <zhangxian887@gmail.com>
2020-12-22 11:27:56 +0800

    增加break的读取

--- reader/csv.go
+++ reader/csv.go
@@ -3,7 +3,34 @@ package reader
 import (
 	"encoding/csv"
 	"io"
+	"os"
 )
+
+// CsvFromFileWithBreak 从文件中读取，允许中断 true为中断信号
+func CsvFromFileWithBreak(name string, f func(rcs []string) bool) error {
+	file, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return CsvWithBreak(file, f)
+}
+
+// CsvWithBreak 从csv io.Reader 中读取，允许中断 true为中断信号
+func CsvWithBreak(r io.Reader, f func(rcs []string) bool) error {
+	cr := csv.NewReader(r)
+	for {
+		rcs, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		f(rcs)
+	}
+	return nil
+}
 
 // Csv 读取解析csv内容
 func Csv(r io.Reader, f func(rcs []string)) error {
--- reader/file.go
+++ reader/file.go
@@ -3,7 +3,18 @@ package reader
 import (
 	"bufio"
 	"io"
+	"os"
 )
+
+// ReadLinesFromFileWithBreak 从文件中读取行 允许中断 true为中断信号
+func ReadLinesFromFileWithBreak(name string, f func(line []byte) bool) error {
+	file, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return ReadLinesWithBreak(file, f)
+}
 
 // ReadLines 按行读取
 func ReadLines(r io.Reader, f func(line []byte)) error {
@@ -17,6 +28,26 @@ func ReadLines(r io.Reader, f func(line []byte)) error {
 			return err
 		}
 		f(data)
+	}
+	return nil
+}
+
+// ReadLinesWithBreak 按行读取 允许中断 true为中断信号
+func ReadLinesWithBreak(r io.Reader, f func(line []byte) bool) error {
+	br := bufio.NewReader(r)
+	for {
+		data, err := br.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		b := f(data)
+		if b {
+			break
+		}
+
 	}
 	return nil
 }
